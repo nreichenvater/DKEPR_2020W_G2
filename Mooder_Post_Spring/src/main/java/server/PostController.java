a9af4f23c229c0e8f9b4a161fa7410f30098a6d6
jakob <jakob@Jakobs-Laptop>
2020-12-29 10:56:49 +0100

    createPost added

--- Mooder_Post_Spring/src/main/java/server/PostController.java
+++ Mooder_Post_Spring/src/main/java/server/PostController.java
@@ -1,6 +1,8 @@
 package server;
 
+import java.io.IOException;
 import java.net.http.HttpClient;
+
 import java.util.List;
 
 import javax.websocket.OnError;
@@ -14,10 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.google.gson.Gson;
+import com.squareup.okhttp.*;
+
 
 import data.Post;
 @RestController
 public class PostController {
+	private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
 	public static PostDao postDao;
 	public static Post post;
 	private HttpClient httpClient;
@@ -43,5 +48,19 @@ public class PostController {
 	 public void create(@RequestBody Post post) {
 	  postDao.addPost(post);
 	 }
-	
+	private static void createPost() {
+		OkHttpClient httpClient = new OkHttpClient();
+		String jsonString = "{\"ip\":\"localhost\",\"port\":\"8083\",\"type\":\"post\"}";
+		RequestBody body = RequestBody.create(JSON, jsonString);
+        Request request = new Request.Builder()
+            .url("http://localhost:1234/service")
+            .post(body)
+            .build();
+        Response response;
+		try {
+			response = httpClient.newCall(request).execute();
+		} catch (IOException e) {
+			System.out.println("geht ned");
+		}
+	}
 }
